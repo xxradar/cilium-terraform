0b693491e6ef066f946bd93a16e553dfb0472523
xxradar <xxradar@radarhack.com>
2024-04-08 11:30:43 +0200

    update image

--- main.tf
+++ main.tf
@@ -1,17 +1,26 @@
 resource "kind_cluster" "this" {
   name = "cilium-testing-terraform"
-  
- kind_config {
+
+  kind_config {
     kind        = "Cluster"
     api_version = "kind.x-k8s.io/v1alpha4"
 
     node {
       role = "control-plane"
+      image = "kindest/node:v1.29.2"
     }
 
     node {
       role = "worker"
+      image = "kindest/node:v1.29.2"
     }
+
+    node {
+      role = "worker"
+      image = "kindest/node:v1.29.2"
+    }
+
+
 
     networking {
       disable_default_cni = true
@@ -25,5 +34,5 @@ resource "cilium" "this" {
     "operator.replicas=1",
     "tunnel=vxlan",
   ]
-  version = "1.14.5"
+  version = "1.15.3"
 }
